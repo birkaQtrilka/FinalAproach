196f0bb6cacf1660662e5d9760150f33b0e2b5c0
Birka <birkavidit@gmail.com>
2024-05-14 13:45:23 +0200

    Added arrow visual, gameManager, draggable blocks

---
+++ gxpengine_template/MyClasses/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using gxpengine_template.MyClasses.Dragging;
+using gxpengine_template.MyClasses.Environment;
+using gxpengine_template.MyClasses.UI;
+using System.Collections;
+using TiledMapParser;
+
+namespace gxpengine_template.MyClasses
+{
+    public class GameManager : TiledGameObject
+    {
+        public static GameManager Instance { get; private set; }
+
+        Player _player;
+        PlayerLauncher _launcher;
+        StarsUI _starsUI;
+
+        public GameManager(string filename, int cols, int rows, TiledObject data) : base(filename, cols, rows, data)
+        {
+            if(Instance != null && Instance == this)
+            {
+                Destroy();
+                return;
+            }
+            else 
+                Instance = this;
+            AddChild(new Coroutine(Init()));
+        }
+
+        IEnumerator Init()
+        {
+            yield return null;
+            _player = MyUtils.MyGame.CurrentLevel.FindObjectOfType<Player>();
+            _launcher = MyGame.main.FindObjectOfType<PlayerLauncher>();
+            _starsUI = MyGame.main.FindObjectOfType<StarsUI>();
+        }
+
+        public void StartIdleMode()
+        {
+            _player.SetIdleMode();
+            _player.SetPosInVec2(_player.StartPos);
+            foreach (var star in _starsUI.Stars)
+                star.ReEnable();
+            _starsUI.ResetScore();
+            Dragger.Instance.CanDrag = true;
+            _launcher.CanLaunch = true;
+        }
+
+        protected override void OnDestroy()
+        {
+            Instance = null;
+        }
+    }
+}
--- gxpengine_template/MyClasses/UI/Buttons/ChangePlayModeBtn.cs
+++ gxpengine_template/MyClasses/UI/Buttons/ChangePlayModeBtn.cs
@@ -8,34 +8,14 @@ namespace gxpengine_template.MyClasses.UI
 {
     public class ChangePlayModeBtn : Button
     {
-        /*temporary for testing*/
-        Player _player;
-        PlayerLauncher _launcher;
-        StarsUI _starsUI;
 
         public ChangePlayModeBtn(string filename, int cols, int rows, TiledObject data) : base(filename, cols, rows, data)
         {
-            AddChild(new Coroutine(Init()));
-        }
-
-        IEnumerator Init()
-        {
-            yield return null;
-            _player = MyUtils.MyGame.CurrentLevel.FindObjectOfType<Player>();
-            _launcher = MyGame.main.FindObjectOfType<PlayerLauncher>();
-            _starsUI = MyGame.main.FindObjectOfType<StarsUI>();
         }
 
         protected override void OnButtonPress()
         {
-            //put this in GameManager?
-            _player.SetIdleMode();
-            _player.SetPosInVec2(_player.StartPos);
-            foreach (var star in _starsUI.Stars)
-                star.ReEnable();
-            _starsUI.ResetScore();
-            Dragger.Instance.CanDrag = true;
-            _launcher.CanLaunch = true;
+            GameManager.Instance.StartIdleMode();
         }
     }
 }
