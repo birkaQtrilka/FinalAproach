5a44cd26d2d7f8531ad3cb389dc09096b9f9e7fb
LluisAlguersuari <lluisalguersuari@gmail.com>
2024-05-16 15:18:55 +0200

    added half act 2 levels

--- gxpengine_template/MyClasses/SceneHandling/MyGame.cs
+++ gxpengine_template/MyClasses/SceneHandling/MyGame.cs
@@ -27,7 +27,7 @@ public class MyGame : Game
         //to show how fast you've beat the game at the end
         StartTime = DateTime.Now;
         
-        LoadLevel("Assets/RectTest.tmx");
+        LoadLevel("Assets/MainMenuScreen.tmx");
 
         OnAfterStep += LoadSceneIfNotNull;
 	}
